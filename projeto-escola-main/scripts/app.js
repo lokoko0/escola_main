2646f6ee787ed08f07f526f3b48e956c9b5aa80b
lokoko0 <110237129+lokoko0@users.noreply.github.com>
2023-04-27 17:00:39 -0300

    Delete projeto-escola-main directory

--- projeto-escola-main/scripts/app.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById("l_mode");
-
-button.addEventListener("click", () => {
-  if (document.body.style.color == "black") {
-    document.body.style.color = "white";
-  } else {
-    document.body.style.color = "black";
-  }
-});
